187183f5cdf7ab722cb661478f9faf2ef4acea3f
kspurgin <kspurgin@email.unc.edu>
2017-01-22 20:26:18 -0500

    prepare _endeca_prepipeline_name_to_element.tsv

--- endeca_prepipeline_name_to_element.rb
+++ endeca_prepipeline_name_to_element.rb
@@ -1,59 +1,50 @@
-outlines = ["Element name\tEndeca dmod name\tFacet on element?\tIndexed in"]
+outlines = ["Prepipeline label\tElement name\tEndeca dmod name\tFacet on element?\tIndexed in"]
 
 pp_data = {}
 File.open('endeca_prepipeline_name_to_dmod_element.tsv', 'r').each_line { |ln|
   ln.chomp!
   if ln.start_with?('Prepipeline Label')
     next
+  elsif /^$/.match(ln)
+    next
   else
     m = /^(.*?)\t(.*)$/.match(ln)
     pplabel = m[1]
-    element = m[2]
+    dmodname = m[2]
     if pp_data.has_key?(pplabel)
-      pp_data[pplabel] << element
+      pp_data[pplabel] << dmodname
     else
-      pp_data[pplabel] = [element]
+      pp_data[pplabel] = [dmodname]
     end
   end
 }
 
-puts pp_data.inspect
-# dim_list = []
-# File.open('endeca_dimensions.tsv', 'r').each_line { |ln|
-#   ln.chomp!
-#   if ln.start_with?('Dimension name')
-#     next
-#   else
-    
-#     dim_list << m[1]
-#   end
-# }
 
-# File.open('endeca_final_elements.tsv', 'r').each_line { |ln|
-#   ln.chomp!
-#   if ln.start_with?('Endeca element name')
-#     next
-#   else
-#     m = /^(.*?)\t(.*)$/.match(ln)
-#     element = m[1]
-#     dmodname = m[2]
-#     if dim_list.include?(element)
-#       facet = 'yes'
-#     else
-#       facet = 'no'
-#     end
-#     if ind_data.has_key?(element)
-#       indin = ind_data[element]
-#     else
-#       indin = 'not indexed'
-#     end
-#   end
-#     outlines << "#{element}\t#{dmodname}\t#{facet}\t#{indin}"
-# }
+el_data = {}
+File.open('_endeca_final_elements_compiled.tsv', 'r').each_line { |ln|
+  ln.chomp!
+  if ln.start_with?('Element name')
+    next
+  elsif /^$/.match(ln)
+    next
+  else
+    m = /^(.*?)\t(.*?)\t(.*)$/.match(ln)
+    dmodname = m[2]
+    other = [m[1], m[3]]
+    el_data[dmodname] = other
+  end
+}
 
+pp_data.each_pair { |ppl, dmodname|
+  dmodname.each { |dn|
+    puts "#{ppl.inspect}: #{dn.inspect}"
+    edata = el_data[dn]
+    outlines << "#{ppl}\t#{edata[0]}\t#{dn}\t#{edata[1]}"
+  }
+}
 
-#  open('_endeca_final_elements_compiled.tsv', 'w') { |f|
-#    outlines.each { |ln|
-#      f.puts ln
-#    }
-#  }
+ open('_endeca_prepipeline_name_to_element.tsv', 'w') { |f|
+   outlines.each { |ln|
+     f.puts ln
+   }
+ }
