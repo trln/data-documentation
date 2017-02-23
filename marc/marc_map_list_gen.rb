75da5e1a869bce72d9f5af55d5acb2c982e0ec85
kspurgin <kspurgin@email.unc.edu>
2017-02-22 21:32:53 -0500

    output summary and exploded files

--- marc/marc_map_list_gen.rb
+++ marc/marc_map_list_gen.rb
@@ -1,6 +1,13 @@
 Sfmap = Struct.new(:ee, :tag, :sf, :i1, :i2, :constraint, :inst)
 sfsmapped = []
-tagsmapped = {}
+
+# {tag =>
+#   { summary => [string, string],
+#   { exploded => [array, array]
+# }
+tag_mappings = {}
+tag_mappings['na'] = {:summary => 'not mapped to Endeca', :exploded => [['na', 'na', 'na', 'na', 'na', 'na']]}
+
 File.open('marc_to_endeca.tsv', 'r').each_line { |ln|
   ln.chomp!
   if ln.start_with?('Endeca Element')
@@ -19,31 +26,32 @@ File.open('marc_to_endeca.tsv', 'r').each_line { |ln|
       sfsmapped << Sfmap.new(ee, tag, sf, i1, i2, constraint, inst)
     }
 
-    taginfo = ""
+    # create summary/verbose/one line tag mappings
+    tag_map_summary = ""
+    tag_map_summary << "(#{inst}): "
     if i1 != '.' && i2 != '.'
-      taginfo << "WHEN i1=#{i1} AND i2=#{i2}"
+      tag_map_summary << "WHEN i1=#{i1} AND i2=#{i2}"
     elsif i1 != '.'
-      taginfo << "WHEN i1=#{i1}"
+      tag_map_summary << "WHEN i1=#{i1}"
     elsif i2 != '.'
-      taginfo << "WHEN i2=#{i2}"
+      tag_map_summary << "WHEN i2=#{i2}"
     end
-
-    taginfo << ", " unless taginfo == ''
-    
+    tag_map_summary << ", " unless tag_map_summary.end_with?('): ')
     if constraint != '.'
       if i1 != '.' || i2 != '.'
-        taginfo << " AND #{constraint}, "
+        tag_map_summary << " AND #{constraint}, "
       else
-        taginfo << "WHEN #{constraint}, "
+        tag_map_summary << "WHEN #{constraint}, "
       end
     end
+    tag_map_summary << "#{sfs} -> #{ee}"
 
-    taginfo << "#{sfs} -> #{ee}"
-
-    if tagsmapped.has_key?(tag)
-      tagsmapped[tag] << taginfo
+    # populate tag_mappings hash
+    if tag_mappings.has_key?(tag)
+      tag_mappings[tag][:summary] << ";;;#{tag_map_summary}"
+      tag_mappings[tag][:exploded] << [ee, i1, i2, constraint, sfs, inst]
     else
-      tagsmapped[tag] = [taginfo]
+      tag_mappings[tag] = {:summary => tag_map_summary, :exploded => [[ee, i1, i2, constraint, sfs, inst]]}
     end
   end
 }
@@ -52,27 +60,44 @@ sfsmapped.each { |sf|
 #  puts "#{sf[:tag]} #{sf[:sf]}"
 }
 
-tagsmapped.each_pair { |t, i|
+tag_mappings.each_pair { |t, i|
 #  puts "#{t}: #{i.to_s}"
 }
 
-open('_marc_bib_tags_mapped.tsv', 'w') { |outfile|
-  outlns = []
-  File.open('_marc_bib_tags.tsv', 'r').each_line { |ln|
-    ln.chomp!
-    line = ln.split(/\t/)
-    if line[0] == 'MARC tag'
-      line << 'Endeca mappings'
+
+tag_sum = File.open('_marc_bib_tags_mapped_summary.tsv', 'w')
+tag_expl = File.open('_marc_bib_tags_mapped_exploded.tsv', 'w')
+
+# for each MARC tag listed in standard... 
+File.open('_marc_bib_tags.tsv', 'r').each_line { |ln|
+  ln.chomp!
+  line = ln.split(/\t/)
+  explosion = []
+  if line[0] == 'MARC tag'
+    summ_line = line.clone << 'Endeca mappings'
+    expl_line = line.clone << ['Endeca element', 'subfields mapped', 'i1 constraint', 'i2 constraint', 'other constraint', 'institution']
+    expl_line.flatten!
+    explosion << expl_line
+  else
+    mtag = line[0]
+    if tag_mappings.has_key?(mtag)
+      lkup_tag = mtag
     else
-      t = line[0]
-      if tagsmapped.has_key?(t)
-        maps = tagsmapped[t].join(';;;')
-        line << maps
-      else
-        line << 'not mapped to Endeca'
-      end
+      lkup_tag = 'na'
     end
-    outlns << line
+    summ_line = line.clone << tag_mappings[lkup_tag][:summary]
+    tag_mappings[lkup_tag][:exploded].each { |v|
+      poof = line.clone << v
+      explosion << poof.flatten
+    }
+  end
+
+  explosion.each { |poof|
+    tag_expl.puts poof.join("\t")
   }
-  outlns.each { |ln| outfile.puts ln.join("\t") }
+
+  tag_sum.puts summ_line.join("\t")
 }
+
+tag_sum.close
+tag_expl.close
