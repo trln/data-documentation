bfdaf47e44a62ba13910bc79e07ada77cb82f72f
Kristina Spurgin <kspurgin@email.unc.edu>
2018-10-04 23:19:09 -0400

    refactor MARC spec extractor; clean up non-json files

--- marc/extract_marc_bib_spec.rb
+++ marc/extract_marc_bib_spec.rb
@@ -1,4 +1,5 @@
 require 'net/http'
+require 'json'
 
 htmlsrc = Net::HTTP.get_response(URI('http://www.loc.gov/marc/bibliographic/ecbdlist.html'))
 if htmlsrc.message == 'OK'
@@ -8,133 +9,95 @@ if htmlsrc.message == 'OK'
   data = data.gsub!(/^--Leader.*?\n--/m, '--')
 end
 
-#p data
+class FieldDef
+  attr_reader :tag_def
+  attr_reader :sf_defs
+  attr_reader :tag
 
- data_chunks = data.split(/^\s*$/)
- marcfields = ["MARC tag\tField name\tRepeatability\tOther note"]
- marcsfs = ["MARC tag\tField name\tField repeatability\tField note\tSubfield delimiter\tSubfield name\tSubfield repeatability\tSubfield note"]
-
-data_chunks_in_lines = []
-
-data_chunks.each do |f|
-  lines = f.split(/\n/).each { |ln| ln.chomp! }
-  data_chunks_in_lines << lines.select { |ln| ln !~ /^( *|   Subfield Codes.*|--.*)$/ }  
-end
-
-field_data_in_lines = data_chunks_in_lines.select { |arr| arr.size > 0 }
-
-# A start at improving this code, used for some debugging. Once problem was fixed, didn't have time to refactor the whole thing. Whee. --2017-08-07
-# class Field
-#   attr_reader = :tag
-#   attr_reader = :name
-#   attr_reader = :repeat
-#   attr_reader = :context
-#   attr_reader = :subfields
-
-#   def initialize(flines)
-#     fdata = /(^\d{3}) - ([^(\[]+)/.match(flines.shift)
-#     @tag = fdata[1]
-#     @name = fdata[2]
-#     @repeat = ''
-#     @context = ''
-#     @subfields = []
-
-#     puts "#{@tag} -- #{@name}"
-#   end
-# end
-
-# #field_data_in_lines.each { |fd| Field.new(fd) }
-
-# class Subfield
-#   attr_reader = :tag
-#   attr_reader = :name
-#   attr_reader = :repeat
-#   attr_reader = :context
-# end
-
-
-
- data_chunks.each do |f|
-  lines = f.split(/\n/)
-  lines.each { |ln| ln.chomp! }
-  keeplines = []
-  lines.each do |ln|
-    if ln.match(/^$/)
-      next
-    elsif ln.match(/^   Subfield Codes/)
-      next
-    else
-      keeplines << ln
-    end
-  end
-
-  if keeplines.size == 0
-    next
-  else
-    fielddata = ''
-    inddata = []
-    sfdata = []
-    keeplines.each do |ln|
-      if ln.match(/^\d{3}/)
-        fielddata = ln
-      elsif ln.match(/^      \$/)
-        sfdata << ln
+  def initialize(field_def)
+    lines = field_def.split(/\n/).each { |ln| ln.chomp! }
+    @sf_defs = []
+    lines.each do |line|
+      if line =~ /^[0-9]{3} - /
+        @tag_def = line
+        @tag = line.byteslice(0,3)
+      elsif line =~ /^ +\$. - /
+        @sf_defs << line
       end
     end
   end
-
-  ftag = fielddata[0, 3]
-  fname = / - (.*?) \(/.match(fielddata)[1]
-  frep = ''
-  fcontext = ''
-  if /\((N?R)\)/.match(fielddata)
-    frep = /\((N?R)\)/.match(fielddata)[1]
-  end
-  frep = '.' if frep == ''
-  if /(\[.+\])/.match(fielddata)
-    fcontext = /(\[.+\])/.match(fielddata)[1]
-  end
-  fcontext = '.' if fcontext == ''
-  marcfields << "#{ftag}\t#{fname}\t#{frep}\t#{fcontext}"
-
-  sfdata.each { |ln|
-    sfdelim = ''
-    sfname = ''
-    sfrep = ''
-    sfmore = ''
-    
-    sfdelim = /\$(.|.-.) (-|S)/.match(ln)[1]
-    if /\$. - .* \(/.match(ln)
-      sfname = /\$. - (.*) \(/.match(ln)[1]
-    elsif /\$.-. S/.match(ln)
-      sfmame = /\$.-. (S.*)/.match(ln)[1]
-    else
-      sfname = ''
-    end
-    if /\(N?R\)/.match(ln)
-      sfrep = /\((N?R)\)/.match(ln)[1]
-    else
-      sfrep = '.'
-    end
-    if /\[.+\]/.match(ln)
-      sfmore = /(\[.+\])/.match(ln)[1]
-    else
-      sfmore = '.'
-    end
-
-    marcsfs << "#{ftag}\t#{fname}\t#{frep}\t#{fcontext}\t#{sfdelim}\t#{sfname}\t#{sfrep}\t#{sfmore}"
-  }
 end
 
-open('_marc_bib_tags.tsv', 'w') { |f|
-  marcfields.each { |mf|
-    f.puts mf
-  }
-}
+class Field
+  attr_reader :tag
+  attr_reader :name
+  attr_reader :repeat
+  attr_reader :context
+  attr_accessor :subfields
 
-open('_marc_bib_subfields.tsv', 'w') { |f|
-  marcsfs.each { |mf|
-    f.puts mf
-  }
-}
+  def initialize(fdata)
+    fdata = /(^\d{3}) - ([^(\[]+.*)(\(.*\))(.*)$/.match(fdata)
+    @tag = fdata[1]
+    @name = fdata[2]
+    @repeat = fdata[3].delete("()") if fdata[3] =~ /^\(N?R\)$/
+    @context = fdata[4].delete("[] -")
+    @subfields = {}
+  end
 
+  def to_h
+    { @tag => {
+        "name" => @name,
+        "repeat" => @repeat,
+        "context" => @context,
+        "subfields" => @subfields
+      }
+    }
+  end
+end
+
+class Subfield
+  attr_reader :code
+  attr_reader :name
+  attr_reader :repeat
+  attr_reader :context
+
+  def initialize(sf_def)
+    sfdata = /^ +\$(.) - ([^(\[]+).*/.match(sf_def)
+    @code = sfdata[1]
+    @name = sfdata[2].strip
+    if sf_def =~ /\(N?R\)/
+      rpt = /\((N?R)\)/.match(sf_def)
+      @repeat = rpt[1]
+    else
+      @repeat = ""
+    end
+    if sf_def =~ /\[.+\] *$/
+      cxt = /\[(.*)\] *$/.match(sf_def)
+      @context = cxt[1].delete("[] -")
+    else
+      @context = ''
+    end
+  end
+end
+
+field_defs = data.split(/^\s*$/)
+defs = field_defs.map { |fd| FieldDef.new(fd) }
+
+fieldhash = {}
+fields = defs.map { |d| Field.new(d.tag_def).to_h }.each do |f|
+  f.each { |k, v| fieldhash[k] = v }
+end
+
+defs.each do |d|
+  d.sf_defs.each do |sfd|
+    sf = Subfield.new(sfd)
+    sfh = {
+      "name" => sf.name,
+      "repeat" => sf.repeat,
+      "context" => sf.context
+    }
+    fieldhash[d.tag]['subfields'][sf.code] = sfh
+  end
+end
+
+open('marc_definition.json', 'w'){ |f| f.puts JSON.pretty_generate(fieldhash)}
